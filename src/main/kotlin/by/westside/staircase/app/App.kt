090bd2aa77dffd61efe85dc39bcb2254496ec6eb
d.pranchuk <d.pranchuk@gmail.com>
2016-03-29 16:56:05 +0300

    Kotlin was updated to 1.0.0

--- src/main/kotlin/by/westside/staircase/app/App.kt
+++ src/main/kotlin/by/westside/staircase/app/App.kt
@@ -1,9 +1,8 @@
 package by.westside.staircase.app
 
-import by.westside.staircase.core.http.request.RequestType
 import by.westside.staircase.core.http.response.HttpResponse
 import by.westside.staircase.core.server.startSyncServer
-import java.util.*
+import java.util.Date
 
 /**
  * Created by d.pranchuk on 1/20/16.
@@ -13,11 +12,5 @@ fun main(args: Array<String>) {
     syncServer.get("/hello", { request ->
         Thread.sleep(1000)
         HttpResponse("Hello! Now is ${Date().time}")
-    })
-    syncServer.registerListener("/patch", RequestType.PATCH, { request ->
-        request.body
-    })
-    syncServer.get("/loaderio-92dba1a89b55cf86eb150e1ecf14aee0/", { req ->
-        HttpResponse("loaderio-92dba1a89b55cf86eb150e1ecf14aee0")
     })
 }
